529fbb8ed4442d528df181d0e57dd699107753b5
imWearingPJs <michaelkozub@gmail.com>
2019-02-07 12:10:26 -0700

    copyright

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
+//  Created by Michael Kozub on 07/02/2019.
+//  Copyright © Michael Kozub. All rights reserved.
 //
 
 import UIKit
